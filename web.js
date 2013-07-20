c217b276504c7bb508f45d1f5628365f522aa73e
Chirag Su <sevenitoff@gmail.com>
2013-07-20 05:25:04 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -2,11 +2,19 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+/*
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+*/
+
+app.get('/', function(request, response) {
+var k = fs.readFileSync('./index.html','utf8');
+ response.send( k );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
